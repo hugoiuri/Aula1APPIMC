16cc5b4804a861848415d5534a908dac4571e550
Hugo Iuri Cordeiro <525977@sga.pucminas.br>
2018-05-24 22:31:40 -0300

    parcial

--- app/src/main/java/pucminas/br/aula1appimc/MainActivity.java
+++ app/src/main/java/pucminas/br/aula1appimc/MainActivity.java
@@ -2,6 +2,9 @@ package pucminas.br.aula1appimc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void CalcularIMC(View view){
+        TextView lblIMC = findViewById(R.id.lblIMCId);
+        EditText txtPeso = findViewById(R.id.txtPesoId);
+        EditText txtAltura = findViewById(R.id.txtAlturaId);
+        double peso = Double.valueOf(txtPeso.getText().toString());
+        double altura = Double.parseDouble(txtAltura.getText().toString())/100;
+        double imc =  Math.round(peso/(altura*altura));
+        String result = String.valueOf(imc);
+        lblIMC.setText(result);
+    }
+
+    // public void getStatus(double imc)
+
 }
