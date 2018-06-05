b5453579a7744a8b01a2a52020a3c0898d40fa3e
hugoiuri <hugoraiuri@gmail.com>
2018-06-05 17:47:24 -0300

    adicionando classificação de imc e ajustes no layout

--- app/src/main/java/pucminas/br/aula1appimc/MainActivity.java
+++ app/src/main/java/pucminas/br/aula1appimc/MainActivity.java
@@ -16,15 +16,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void CalcularIMC(View view){
         TextView lblIMC = findViewById(R.id.lblIMCId);
+        TextView lblIMCClassificacao = findViewById(R.id.lblIMCClassificacaoId);
         EditText txtPeso = findViewById(R.id.txtPesoId);
         EditText txtAltura = findViewById(R.id.txtAlturaId);
+
         double peso = Double.valueOf(txtPeso.getText().toString());
         double altura = Double.parseDouble(txtAltura.getText().toString())/100;
-        double imc =  Math.round(peso/(altura*altura));
+        double imc =  calcularIMC(peso, altura);
+
         String result = String.valueOf(imc);
+        String classificacao = getClassificacao(imc);
+
         lblIMC.setText(result);
+        lblIMCClassificacao.setText(classificacao);
     }
 
-    // public void getStatus(double imc)
+    private double calcularIMC(double peso, double altura){
+        double imc =  Math.round(peso/(altura*altura));
 
+        return imc;
+    }
+
+    private String getClassificacao(double imc){
+        if(imc < 16){
+            return "Magreza grave";
+        } else if (imc < 17){
+            return "Magreza moderada";
+        } else if (imc < 18.5){
+            return "Magreza leve";
+        } else if (imc < 25){
+            return "Saudável";
+        } else if (imc < 30){
+            return "Sobrepeso";
+        } else if (imc < 35){
+            return "Obesidade Grau I";
+        } else if (imc < 40){
+            return "Obesidade Grau II (severa)";
+        }
+        return "Obesidade Grau III (mórbida)";
+    }
 }
